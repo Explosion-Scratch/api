252577632d61383f08ed94fcb16ba273b5dd9bea
--Explosion-- <explosion.scratch@gmail.com>
2021-03-08 12:27:41 +0000

    Cool stuff

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ app.use(function(req, res, next){
    trycatch(function(){
            app.router(req, res, next);
        }, function(er){
-					 res.send("Internal server errror")
-           res.sendStatus(500);
+				  res.status(500);
+					res.json("Internal server error")
        });
 });
+
 app.set("json spaces", 2);
 app.use(bodyParser.json({strict: false, type: "*/*"}));
 
 console.log = () => {};
 
 app.get("/", (req, res) => {
-	throw new Error("Test")
   res.send(
     "API list (use '/apiname/help' to get info regarding a specific one):<br><br>translate: Translate text.<br>google: Google search text and get a JSON response.<br>emoji: Emojify strings that contain stuff like ':heart:'. Supports multiple endpoints<br>beautify: Beautify code using prettier.<br>uuid: Generate uuids in bulk (up to 5000 a request)<br>markdown: Convert html or markdown to markdown or html.<br>randexp: Generate a string that matches a regular expression.<br>image-search: Searches google images for a query, also supports direct linking to the 1st search result."
   );
